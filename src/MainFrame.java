62814447240cc05f09b80dc4fb330fc59756390c
Hans de los Santos <hansd4@nycstudents.net>
2023-04-12 10:10:02 -0400

    Initial commit

---
+++ src/MainFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class MainFrame {
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+}
---
+++ src/SaveFile.java
@@ -0,0 +1,13 @@
+import java.io.Serial;
+
+public class SaveFile implements java.io.Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    // put data here
+
+
+    public SaveFile() {
+        // set data here
+    }
+}
